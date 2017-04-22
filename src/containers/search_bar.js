dd4204be0bfb6fe9b1cec9a95489a7b030c9f599
Paul Simon Ongpin <paulsimon.ongpin@Pauls-MacBook-Pro.local>
2017-04-22 16:06:00 +0800

    Add Component State to Seachbar Component

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: '' };
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  onInputChange(event){
+      console.log(event.target.value);
+      this.setState({ term: event.target.value });
+  }
+
   render() {
     return (
       <form className="input-group">
-        <input type="text" />
+        <input
+          placeholder="Get your 5-day forcast in your favorite cities"
+          className="form-control"
+          value={this.state.term}
+          onChange={this.onInputChange}
+        />
         <span className="input-group-btn">
           <button className="btn btn-secondary">Submit</button>
         </span>
