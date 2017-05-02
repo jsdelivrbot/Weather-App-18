0df32bb10cb8296b22cc990c8f6cf20ffb05a940
Paul Simon Ongpin <paulsimon.ongpin@Pauls-MacBook-Pro.local>
2017-05-02 23:04:47 +0800

    Create AJAX Request using axios for weather data

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,16 @@
-conts API_KEY = 'dc7c62e3b93ca0c9703feeb4339673f2';
+import axios from 'axios';
+
+const API_KEY = 'dc7c62e3b93ca0c9703feeb4339673f2';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?&appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
